04fd4752d35935fad72a10748fc56eeddd079c85
Zhan Yi <liauzhanyi@gmail.com>
2024-03-10 16:56:02 +0800

    Add list of api users

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import DisplayTable from "./components/DisplayTable";
 
 function App() {
-    const [authors, setAuthors] = useState([]);
+    const [authors, setAuthors] = useState<string[]>([]);
+    const [users, setUsers] = useState<string[]>([]);
 
-    const addAuthor = (author) => {
-        setAuthors((prevAuthors) => [...prevAuthors, author]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((data) =>
+                setUsers(data.map((user: { name: string }) => user.name))
+            );
+    }, []);
+
+    const addAuthor = (author: string) => {
+        setAuthors((prevAuthors: string[]) => [...prevAuthors, author]);
     };
 
     return (
         <>
             <Form lbl="author name" type="text" onSubmit={addAuthor} />
             <DisplayTable lbl="Author Names Submitted" items={authors} />
+            <DisplayTable lbl="List of Users" items={users} />
         </>
     );
 }
