20814b74eeb23833b94dd14ecbbce6a6d3213523
Zhan Yi <liauzhanyi@gmail.com>
2024-03-10 16:47:48 +0800

    Update display table with submitted names

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import DisplayTable from "./components/DisplayTable";
 
 function App() {
+    const [authors, setAuthors] = useState([]);
+
+    const addAuthor = (author) => {
+        setAuthors((prevAuthors) => [...prevAuthors, author]);
+    };
+
     return (
         <>
-            <Form lbl="author name" type="text" />
-            <DisplayTable
-                lbl="Author Names Submitted"
-                items={["test1", "test2"]}
-            />
+            <Form lbl="author name" type="text" onSubmit={addAuthor} />
+            <DisplayTable lbl="Author Names Submitted" items={authors} />
         </>
     );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface FormProps {
     lbl: string;
     type: string;
+    onSubmit: (value: string) => void;
 }
 
-const Form: React.FC<FormProps> = ({ lbl, type }) => {
+const Form: React.FC<FormProps> = ({ lbl, type, onSubmit }) => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleSubmit = (event: { preventDefault: () => void }) => {
+        event.preventDefault();
+        onSubmit(inputValue);
+        setInputValue("");
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>{lbl}</label>
-            <input type={type} />
+            <input
+                type={type}
+                value={inputValue}
+                onChange={(elem) => setInputValue(elem.target.value)}
+            />
             <button type="submit">Submit</button>
         </form>
     );
